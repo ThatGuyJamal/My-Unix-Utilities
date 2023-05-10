12b6824541c02fd3d7a5b71ad08f940d5f64174d
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-05-10 17:41:50 -0400

    Update config.fish

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # Disables the fish default greet message on startup.
+    set fish_greeting "" 
+
     set PATH $PATH ~/.cargo/bin
     starship init fish | source
 end
