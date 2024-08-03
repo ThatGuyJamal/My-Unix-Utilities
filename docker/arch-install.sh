36a2ec76271a5eea5da061f08f8b9692572c4f78
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-08-03 12:45:57 -0400

    Update arch-install.sh
    Adding logic for installer cleanup

--- docker/arch-install.sh
+++ docker/arch-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILE_NAME=docker-desktop-x86_64.pkg.tar.zst
+TEMP_DIR=$(mktemp -d)
 
 sudo pacman -S gnome-terminal # Required for docker
 sudo pacman -S docker -y
@@ -9,13 +10,16 @@ sudo pacman -S docker -y
 sudo usermod -aG docker $(whoami)
 su $(whoami)
 
-# Download Docker Desktop package
-wget https://desktop.docker.com/linux/main/amd64/$FILE_NAME
+# Download Docker Desktop package to the temporary directory
+wget -P $TEMP_DIR https://desktop.docker.com/linux/main/amd64/$FILE_NAME
 
-# Extract the package
-tar --use-compress-program=unzstd -xvf $FILE_NAME
+# Extract the package in the temporary directory
+tar --use-compress-program=unzstd -xvf $TEMP_DIR/$FILE_NAME -C $TEMP_DIR
 
 # Install Docker Desktop
-sudo pacman -U --noconfirm $FILE_NAME
+sudo pacman -U --noconfirm $TEMP_DIR/$FILE_NAME
 
-echo "Docker and Docker Desktop has been installed!"
+echo "Docker and Docker Desktop have been installed!"
+
+# Clean up
+rm -rf $TEMP_DIR
